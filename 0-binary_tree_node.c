ccf510027670771d48d09be4cacf80e13642c0dd
NathyG2524 <Nathygirma2524@gmail.com>
2022-02-16 10:15:47 -0800

    betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,15 +12,15 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *result;
-    result = malloc(sizeof(binary_tree_t));
-    if (result == NULL)
-    {
-	    return (NULL);
-    }
-    result->left = NULL;
-    result->right = NULL;
-    result->n = value;
-    result->parent = parent;
-    return (result);
+binary_tree_t *result;
+result = malloc(sizeof(binary_tree_t));
+if (result == NULL)
+{
+	return (NULL);
+}
+result->left = NULL;
+result->right = NULL;
+result->n = value;
+result->parent = parent;
+return (result);
 }
