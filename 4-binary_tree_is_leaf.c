add5be7fdb98b6a62445a8f697f034a301c6522d
NathyG2524 <Nathygirma2524@gmail.com>
2022-02-16 23:53:48 -0800

    free all memory of a tree

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ *
+ * @node: pointer to the node to check
+ *
+ * return: 1 or 2 int
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+	if (((node->left) == NULL)
+	    && ((node->right) == NULL))
+		return (1);
+	return (0);
+}
