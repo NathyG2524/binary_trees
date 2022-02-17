22e2c89f3129cfecc4d12c351935ab9e1ab8abd4
NathyG2524 <Nathygirma2524@gmail.com>
2022-02-17 01:46:57 -0800

    return depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -12,10 +12,11 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth;
+
 	if ((tree == NULL) || (tree->parent == NULL))
 	{
-		return 0;
+		return (0);
 	}
 	depth = 1 + binary_tree_depth(tree->parent);
-	return depth;
+	return (depth);
 }
